2cf0d9cc7a9802b371a41350c56837eed037f2d5
SorerBOT <alonfiller@gmail.com>
2023-04-13 21:43:49 +0300

    (Feat) printList, removeValueFromList

--- Main.java
+++ Main.java
@@ -6,14 +6,25 @@ public class Main {
     Node<Integer> current = first;
     Node<Integer> previous = first;
 
-    for (int i = 0; i < 10; i++) {
-      current = new Node(i);
+    for (int i = 0; i < 12; i++) {
+      current = new Node(i % 3);
       previous.setNext(current);
       previous = current;
     }
-    System.out.println(getNumOfNodes(first));
-    AddElement(123, first);
-    System.out.println(getNumOfNodes(first));
+    printList(first); 
+    System.out.printf("The list has: %d items.\n", getNumOfNodes(first));
+    removeValueFromList(first, 10);
+    removeValueFromList(first, 2);
+    printList(first); 
+    System.out.printf("The list has: %d items.\n", getNumOfNodes(first));
+  }
+  private static void printList(Node<Integer> first) {
+    System.out.print("[");
+    while (first.getNext() != null) {
+      System.out.printf("%d, ", first.getValue());
+      first = first.getNext();
+    }
+    System.out.printf("%d]\n", first.getValue());
   }
   private static Node<Integer> AddElement(int value, Node<Integer> current) {
     return new Node<Integer>(value, current);
@@ -30,11 +41,34 @@ public class Main {
   }
   public static int getNumOfNodes(Node<Integer> first) {
     int count = 0;
-    while (first.getNext() != null) {
+    while (first != null) {
       count++;
       first = first.getNext();
     }
 
     return count;
+  }
+  public static Node<Integer> removeValueFromList(Node<Integer> first, int target) {
+    Node<Integer> current = first;
+    Node<Integer> next; 
+    
+
+    while (current.getValue() == target) current = current.getNext();
+    
+    next = current.getNext();
+    first = current;
+
+    while (next != null) {
+      if (next.getValue() == target) {
+        current.setNext(next.getNext());
+        next = current.getNext();
+      }
+      else {
+        current = next;
+        next = next.getNext();
+      }
+    }
+
+    return first;
   }
 }
